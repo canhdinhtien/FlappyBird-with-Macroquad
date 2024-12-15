3937eb729ee7377d39ec4bd9d6dca09b2fee4408
canhdinhtien <131688279+canhdinhtien@users.noreply.github.com>
2024-12-15 22:26:29 +0700

    Initial commit for my game project

---
+++ src/main.rs
@@ -0,0 +1,177 @@
+use macroquad::prelude::*;
+
+const GRAVITY: f32 = 0.5;
+const JUMP_STRENGTH: f32 = -8.5;
+const PIPE_WIDTH: f32 = 50.0;
+const PIPE_GAP: f32 = 150.0;
+
+#[derive(PartialEq)]
+enum GameState {
+    MainMenu,
+    Playing,
+    GameOver,
+}
+
+#[macroquad::main("Flappy Bird")]
+async fn main() {
+    let mut state = GameState::MainMenu;
+    let mut score = 0;
+    let mut high_scores = 0;
+
+    let mut bird_y = screen_height() / 2.0;
+    let mut bird_velocity = 0.0;
+    let mut pipes = vec![(screen_width(), random_pipe_height())];
+
+    let background_texture: Texture2D = load_texture("background-day.png").await.unwrap();
+    let background_texture2: Texture2D = load_texture("background-night.png").await.unwrap();
+    let bird_texture = load_texture("bird.png").await.unwrap();
+
+    loop {
+        clear_background(SKYBLUE);
+
+        match state {
+            GameState::MainMenu => {
+                
+                draw_text("Flappy Bird", screen_width() / 2.0 - 100.0, screen_height() / 2.0 - 100.0, 50.0, WHITE);
+                if button("Start", screen_width() / 2.0 - 100.0, screen_height() / 2.0) {
+
+                    score = 0;
+                    bird_y = screen_height() / 2.0;
+                    bird_velocity = 0.0;
+                    pipes.clear();
+                    pipes.push((screen_width(), random_pipe_height()));
+
+                    state = GameState::Playing;
+                }
+
+                if button("Exit", screen_width() / 2.0 - 100.0, screen_height() / 2.0 + 50.0) {
+                    break;
+                }
+
+                if is_key_pressed(KeyCode::Enter){
+                    score = 0;
+                    bird_y = screen_height() / 2.0;
+                    bird_velocity = 0.0;
+                    pipes.clear();
+                    pipes.push((screen_width(), random_pipe_height()));
+
+                    state = GameState::Playing;
+                }
+                else if is_key_pressed(KeyCode::Escape){
+                    break;
+                }
+            }
+
+            GameState::Playing => {
+                bird_velocity += GRAVITY;
+                bird_y += bird_velocity;
+
+                if is_key_pressed(KeyCode::Space) || is_key_pressed(KeyCode::Up){
+                    bird_velocity = JUMP_STRENGTH;
+                }
+
+                for (x, _) in &mut pipes {
+                    *x -= 2.0 * (score+6) as f32 / 6.0;
+                }
+
+                if pipes[0].0 < -PIPE_WIDTH {
+                    pipes.remove(0);
+                    pipes.push((screen_width(), random_pipe_height()));
+                    score += 1;
+                }
+
+                if bird_y < 0.0 || bird_y > screen_height() || pipes.iter().any(|(pipe_x, pipe_y)| bird_collides_with_pipe(bird_y, *pipe_x, *pipe_y)) {
+                    state = GameState::GameOver;
+                }
+
+                if (score/3)%2 == 0{ 
+                    draw_texture_ex(
+                        background_texture,
+                        0.0,
+                        0.0,
+                        WHITE,
+                        DrawTextureParams {
+                            dest_size: Some(vec2(screen_width(), screen_height())),
+                            ..Default::default()
+                        },
+                    );
+                }
+                else {
+                    draw_texture_ex(
+                        background_texture2,
+                        0.0,
+                        0.0,
+                        WHITE,
+                        DrawTextureParams {
+                            dest_size: Some(vec2(screen_width(), screen_height())),
+                            ..Default::default()
+                        },
+                    );
+                }
+
+                draw_texture(bird_texture, 100.0 - bird_texture.width() / 2.0, bird_y - bird_texture.height() / 2.0, WHITE);
+
+                for (pipe_x, pipe_y) in &pipes {
+                    draw_rectangle(*pipe_x, 0.0, PIPE_WIDTH, *pipe_y, BLUE);
+                    draw_rectangle(*pipe_x, *pipe_y + PIPE_GAP, PIPE_WIDTH, screen_height(), BLUE);
+                }
+
+                draw_text(&format!("Score: {}", score), 10.0, 20.0, 30.0, WHITE);
+                draw_text(&format!("High Score: {}", high_scores), 10.0, 40.0, 30.0, WHITE);
+            }
+            GameState::GameOver => {
+                
+                if score > high_scores{
+                    high_scores = score;
+                }
+
+                draw_text("Game Over!", screen_width() / 2.0 - 100.0, screen_height() / 2.0 - 100.0, 50.0, RED);
+                draw_text(&format!("Score: {}", score), screen_width() / 2.5 - 100.0, screen_height() / 2.0, 30.0, WHITE);
+                draw_text(&format!("High Score: {}", high_scores), screen_width() / 1.5 - 100.0, screen_height() / 2.0, 30.0, WHITE);
+                if button("Play again", screen_width() / 2.0 - 100.0, screen_height() / 2.0 + 50.0) || is_key_pressed(KeyCode::Enter){
+                    score = 0;
+                    bird_y = screen_height() / 2.0;
+                    bird_velocity = 0.0;
+                    pipes.clear();
+                    pipes.push((screen_width(), random_pipe_height()));
+
+                    state = GameState::Playing;
+                }
+
+                if button("Exit", screen_width() / 2.0 - 100.0, screen_height() / 2.0 + 100.0) || is_key_pressed(KeyCode::Escape){
+                    break;
+                }
+            }
+        }
+
+        next_frame().await;
+    }
+}
+
+fn random_pipe_height() -> f32 {
+    rand::gen_range(100.0, screen_height() - 200.0)
+}
+
+fn bird_collides_with_pipe(bird_y: f32, pipe_x: f32, pipe_y: f32) -> bool {
+    let bird_x = 100.0;
+    let bird_radius = 20.0;
+
+    let pipe_top = pipe_y;
+    let pipe_bottom = pipe_y + PIPE_GAP;
+
+    let bird_in_pipe_x = bird_x + bird_radius > pipe_x && bird_x - bird_radius < pipe_x + PIPE_WIDTH;
+    let bird_in_pipe_y = bird_y - bird_radius < pipe_top || bird_y + bird_radius > pipe_bottom;
+
+    bird_in_pipe_x && bird_in_pipe_y
+}
+
+fn button(text: &str, x: f32, y: f32) -> bool {
+    let mouse_pos = mouse_position();
+    let button_rect = Rect::new(x, y, 200.0, 40.0);
+
+    let is_hovered = button_rect.contains(Vec2::new(mouse_pos.0, mouse_pos.1));
+    draw_rectangle(x, y, 200.0, 40.0, if is_hovered { GRAY } else { DARKGRAY });
+    draw_text(text, x + 10.0, y + 25.0, 30.0, WHITE);
+
+    is_hovered && is_mouse_button_pressed(MouseButton::Left)
+}
